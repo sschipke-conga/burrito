1cd397f2f7c54590b03153ff045f86d12acc011d
Scott Schipke <swschipke@gmail.com>
2019-11-06 10:28:50 -0700

    Add tests for handleIngredientsChange

--- src/components/OrderForm/OrderForm.test.js
+++ src/components/OrderForm/OrderForm.test.js
@@ -22,11 +22,43 @@ describe('OrderForm', () => {
     expect(wrapper).toMatchSnapshot()
   })
   describe('handleNameChange', () => {
-    const mockEvent = {target:{name:'name', value:'Steve'}, preventDefault: jest.fn()}
+    const mockEvent = {target:{name:'name', value:'Steve'}}
     it('should set the state if if there is a change', () => {
       expect(wrapper.state('name')).toEqual('')
       wrapper.find('input').simulate('change', mockEvent)
       expect(wrapper.state('name')).toEqual('Steve')
+    })
+  })
+  describe('handleIngredientChange', () => {
+    const mockEvent = {
+      target : {name: 'queso'},
+      preventDefault: jest.fn()
+    }
+    it('should prevent the default behavior', () => {
+      wrapper.instance().handleIngredientChange(mockEvent)
+      expect(mockEvent.preventDefault).toHaveBeenCalled()
+    })
+    it('should update the state with ingredients onClick' ,() => {
+      expect(wrapper.state('ingredients')).toEqual([])
+      wrapper.find('button').at(2).simulate('click', mockEvent)
+      expect(wrapper.state('ingredients')).toEqual(['queso'])
+    })
+    it('should update state with multiple ingredients', () => {
+      const otherMockEvent = {
+        target: { name: 'carnitas' },
+        preventDefault: jest.fn()
+      }
+      const anotherMockEvent = {
+        target: { name: 'peppers' },
+        preventDefault: jest.fn()        
+      }
+      expect(wrapper.state('ingredients')).toEqual([])
+      wrapper.instance().handleIngredientChange(mockEvent)
+      expect(wrapper.state('ingredients')).toEqual(['queso'])
+      wrapper.instance().handleIngredientChange(otherMockEvent)
+      expect(wrapper.state('ingredients')).toEqual(['queso', 'carnitas'])
+      wrapper.instance().handleIngredientChange(anotherMockEvent)
+      expect(wrapper.state('ingredients')).toEqual(['queso', 'carnitas', 'peppers'])
     })
   })
 })
